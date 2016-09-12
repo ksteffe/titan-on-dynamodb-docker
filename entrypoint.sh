99d13540995e075c4c43939b4975a72c8ee87d7e
Diego Ferri <ferri.diego@gmail.com>
2016-09-12 15:30:58 +0200

    Wait for titan launch.

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,5 +9,10 @@ done
 echo "DynamoDB Storage Backend for Titan 1.0.0 launched"
 
 bin/gremlin-server.sh ${PWD}/conf/gremlin-server/gremlin-server-local.yaml &
+echo "Waiting for Titan 1.0.0 to launch on 8182..."
+
+while ! nc -z localhost 8182; do
+  sleep 0.1
+done
 
 echo "Titan 1.0.0 launched"
